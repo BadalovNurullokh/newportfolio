f92b047880edb102d8d568c3621a867b05866723
BadalovNurullokh <nurulllob@gmail.com>
2026-03-31 02:22:07 +0600

    news

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import Projects from './Project';
-import { delay, motion, px } from 'framer-motion'
+import {motion } from 'framer-motion'
 
 
 const animation = {
